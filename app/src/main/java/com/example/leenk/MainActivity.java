d804a02dab1d5921bc358daf9412d7b751d9e982
nonshas20 <66158475+nonshas20@users.noreply.github.com>
2024-06-26 01:39:09 +0800

    di ko magawa yung cam

--- app/src/main/java/com/example/leenk/MainActivity.java
+++ app/src/main/java/com/example/leenk/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin, btnRegister;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
